a630e53bede8887323b47d647113077cbcc62a46
JonnyKong <jonnykong1996@gmail.com>
2017-10-12 17:49:55 -0400

    Updates

--- Assignment2/starter/cars.c
+++ Assignment2/starter/cars.c
@@ -52,6 +52,38 @@ void parse_schedule(char *file_name) {
  */
 void init_intersection() {
 
+    int i;
+
+    // Init isection mutex
+    for(i = 0; i < 4; ++i) {
+        pthread_mutex_init(isection.quad + i, NULL);
+    }
+
+    // Init lane 
+    for(i = 0; i < 4; ++i) {
+
+        // Init mutex and cond_vars
+        pthread_mutex_init(&(isection.lane[i].lock), NULL);
+        pthread_cond_init(&(isection.lane[i].producer_cv), NULL);
+        pthread_cond_init(&(isection.lane[i].consumer_cv), NULL);
+
+        // Init list heads to NULL
+        isection.lane[i].in_cars = NULL;
+        isection.lane[i].out_cars = NULL;
+
+        // Other variables
+        isection.lane[i].inc = 0;
+        isection.lane[i].passed = 0;
+        
+        // Init buffer
+        isection.lane[i].buffer = malloc(LANE_LENGTH * sizeof(struct car*));
+        isection.lane[i].head = 0;
+        isection.lane[i].tail = 0;
+        isection.lane[i].capacity = LANE_LENGTH;
+        isection.lane[i].in_buf = 0;
+
+    }
+
 }
 
 /**
@@ -64,6 +96,32 @@ void init_intersection() {
  */
 void *car_arrive(void *arg) {
     struct lane *l = arg;
+
+    // While there are cars pending to pass through the line
+    while(l -> in_cars != NULL) {
+
+        pthread_mutex_lock(&(l -> lock));
+
+        // If buffer full, wait for space
+        while(l.in_buf == LANE_LENGTH) {
+            pthread_cond_wait(&(l -> consumer_cv), &(l -> lock));
+        }
+
+        // Get the first car from list in_cars
+        buffer[tail] = l -> in_cars;
+        l -> in_cars = l -> in_cars -> next;
+        tail = (tail + 1) % LANE_LENGTH;
+        
+        // Update lane variables
+        ++(l -> inc);
+        ++(l -> in_buf);
+
+        // Signal producer_cv
+        pthread_cond_signal(&(l -> producer_cv));
+
+        pthread_mutex_lock(&l.lock);
+
+    }
 
     /* avoid compiler warning */
     l = l;
@@ -109,9 +167,40 @@ void *car_cross(void *arg) {
  * list of the quadrants the car will pass through.
  * 
  */
-int *compute_path(enum direction in_dir, enum direction out_dir) {
-
-
-
-    return NULL;
+ int *compute_path(enum direction in_dir, enum direction out_dir) {
+    
+    // Need to free memory for list path in outer function
+    
+    // Local Variables
+    int i, j = 0;
+    
+    int *passed = (int *)malloc(4 * sizeof(int));
+    int *path = (int *)malloc(4 * sizeof(int));
+    memset(passed, 0, 4 * sizeof(int));
+    memset(path, 0, 4 * sizeof(int));
+    
+    // Calculate which quadrant to pass, and set the corresponding flag in passed to 1
+    if(in_dir == out_dir) {
+        // Cars make U-turns, takes up all quadrants
+        memset(passed, 1, 4 * sizeof(int));
+    }
+    else{
+        // Otherwise
+        for(i = (in_dir + 1) % 4 + 1; i != out_dir + 2; ++i) {
+            if(i == 5) i = 1;
+            passed[i - 1] = 1;
+        }
+    }
+    
+    // Generate list of passed quadrants. Pad 0s at the end.
+    for(i = 1; i <= 4; ++i) {
+        if(passed[i - 1]) {
+            path[j++] = i;
+        }
+    }
+    
+    // Release Memory and return
+    free(passed);
+    return path;
+    
 }
