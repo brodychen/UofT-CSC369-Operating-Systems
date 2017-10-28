ec8d28acccc2843f5660bdeefe23b90f45114c23
JonnyKong <jonnykong1996@gmail.com>
2017-10-27 23:41:39 -0400

    updates

--- Assignment3/starter/pagetable.c
+++ Assignment3/starter/pagetable.c
@@ -40,6 +40,10 @@ int allocate_frame(pgtbl_entry_t *p) {
 		// Write victim page to swap, if needed, and update pagetable
 		// IMPLEMENTATION NEEDED
 		
+		// Only swap to disk if page is dirty
+		if((p -> frame) & PG_DIRTY) {
+			swap_pagein(p -> frame >> PAGE_SHIFT);
+		}
 
 	}
 
