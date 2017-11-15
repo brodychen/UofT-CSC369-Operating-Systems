501cb67d8b43849060076e454167371ea033debb
JonnyKong <jonnykong1996@gmail.com>
2017-11-15 18:06:42 -0500

    updates

--- TE7/readimage.c
+++ TE7/readimage.c
@@ -27,6 +27,21 @@ int main(int argc, char **argv) {
     struct ext2_super_block *sb = (struct ext2_super_block *)(disk + 1024);
     printf("Inodes: %d\n", sb->s_inodes_count);
     printf("Blocks: %d\n", sb->s_blocks_count);
+    // printf("Superblock size: %d\n", sizeof(struct ext2_super_block));
+    // printf("Inode size: %d\n", sizeof(struct ext2_inode));
+    // printf("First data block: %d\n", sb -> s_first_data_block);
+    // printf("Blocks per group: %d\n", sb -> s_blocks_per_group);
+    // printf("Block group # of this superblock:%d\n", sb -> s_block_group_nr);
+
+    struct ext2_group_desc *gt = (struct ext2_group_desc *)(disk + 2048);
+    printf("Block group:\n");
+    printf("\tblock bitmap: %d\n", gt[0].bg_block_bitmap);
+    printf("\tinode bitmap: %d\n", gt[0].bg_inode_bitmap);
+    printf("\tinode table: %d\n", gt[0].bg_inode_table);
+    printf("\tfree blocks: %d\n", gt[0].bg_free_blocks_count);
+    printf("\tfree inodes: %d\n", gt[0].bg_free_inodes_count);
+    printf("\tused_dirs: %d\n", gt[0].bg_used_dirs_count);
+
     
     return 0;
 }
